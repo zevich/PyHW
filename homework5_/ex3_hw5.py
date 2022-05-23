5616bcb745fa4abbf84cf9defadb537698676237
zevich <105417952+zevich@users.noreply.github.com>
2022-05-24 00:41:12 +0300

    Delete ex3_hw5.py

--- homework5_/ex3_hw5.py
+++
@@ -1,8 +0,0 @@
-new_phone = (phone.strip()
-                .removeprefix('+')
-                .replace('(','')
-                .replace(')','')
-                .replace(' ','')
-                .replace('-','') # Первый Уничтожает пробелы, Второй - префиксы
-    
-    return new_phone
