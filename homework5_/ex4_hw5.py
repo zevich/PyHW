d1c9157a46841a128543377ac8a158ce98399721
zevich <105417952+zevich@users.noreply.github.com>
2022-05-24 00:41:19 +0300

    Delete ex4_hw5.py

--- homework5_/ex4_hw5.py
+++
@@ -1,8 +0,0 @@
-def is_check_name(fullname, first_name):
-    print(fullname)
-    print(first_name)
-    print(fullname.removeprefix(first_name))   # Hook
-    if fullname.removeprefix(first_name) == fullname:
-        return(False)
-    else:
-        return(True)
