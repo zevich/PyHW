c8ba4ea2ec4abe6b47873c3e342b3eafd0113e7b
zevich <105417952+zevich@users.noreply.github.com>
2022-05-24 00:40:28 +0300

    Delete ex8_hw4.py

--- homework4/ex8_hw4.py
+++
@@ -1,8 +0,0 @@
-def game(terra, power):
-    for small_list in terra:
-        for i in small_list:
-            if power >= i:
-                power = power+i
-            else:
-                break
-    return (power)
